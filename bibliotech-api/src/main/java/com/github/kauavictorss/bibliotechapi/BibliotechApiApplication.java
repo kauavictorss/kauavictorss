51530fe8d12376d8f4ba3cf628e012eaf63aad56
Kauã Victor <kaua15321@gmail.com>
2026-07-05 13:20:12 -0300

    feat: Implementa configurações do projeto java e spring

---
+++ bibliotech-api/src/main/java/com/github/kauavictorss/bibliotechapi/BibliotechApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.kauavictorss.bibliotechapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotechApiApplication.class, args);
+	}
+
+}
---
+++ bibliotech-api/src/test/java/com/github/kauavictorss/bibliotechapi/BibliotechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kauavictorss.bibliotechapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
